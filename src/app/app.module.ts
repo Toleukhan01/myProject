18d76acc5af2c4c4bd1097208a218a28b2d71cc7
Narkes <toleuhann01@gmail.com>
2021-09-19 00:46:42 +0600

    blocks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AdsComponent } from './ads/ads.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServiceTypesComponent } from './service-types/service-types.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AdsComponent,
+    AboutUsComponent,
+    ServiceTypesComponent
   ],
   imports: [
     BrowserModule,
